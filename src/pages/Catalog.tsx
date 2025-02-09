6b2653cd93acd1849eaf2cae966b01823e8c8c0a
lolol98483 <aizimbaev2@gmail.com>
2025-02-10 00:15:39 +0400

    Update Catalog.tsx

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -26,10 +26,6 @@ const Catalog = () => {
           
           <div className="flex flex-col sm:flex-row gap-4 w-full md:w-auto">
             <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-            <button className="inline-flex items-center px-4 py-2 border rounded-lg hover:bg-accent/5">
-              <SlidersHorizontal className="h-4 w-4 mr-2" />
-              Filters
-            </button>
           </div>
         </div>
 
